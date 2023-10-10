422ad64f20ff7db3c9f0fab507c6b8d65a1d61d9
seunghyun <tmdgus7820@naver.com>
2023-10-10 22:14:04 +0900

    feat: typescript section3

---
+++ onebite-typescript/section2/src/chapter3.ts
@@ -0,0 +1,22 @@
+// object
+const user: {
+    id: number,
+    name: string
+} = {
+    id: 1,
+    name: "조승현"
+};
+
+const dog: {
+    name: string,
+    color: string
+} = {
+    name: "해피",
+    color: "brown"
+}
+
+const config: {
+    readonly apiKey: string;
+} = {
+    apiKey: "MY API KEY"
+};
