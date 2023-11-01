07f165c7ddb083a8f71c19c56981a97ae2da25c2
seunghyun <tmdgus7820@naver.com>
2023-11-01 23:02:32 +0900

    feat: typescript section6

---
+++ onebite-typescript/section6/src/chapter1.ts
@@ -0,0 +1,55 @@
+/**
+ * 타입스크립트의 클래스
+ */
+
+const employee = {
+    name: '조승현',
+    age: 28,
+    position: 'developer',
+    work() {
+        console.log('일침');
+    },
+};
+
+class Employee {
+    // 필드
+    name: string;
+    age: number;
+    position: string;
+
+    // 생성자
+    constructor(name: string, age: number, position: string) {
+        this.name = name;
+        this.age = age;
+        this.position = position;
+    }
+
+    // 메서드
+    work() {
+        console.log('일침');
+    }
+}
+
+class ExecutiveOfficer extends Employee {
+    // 필드
+    officeNumber: number;
+
+    // 생성자
+    constructor(name: string, age: number, position: string, officeNumber: number) {
+        super(name, age, position);
+        this.officeNumber = officeNumber;;
+    }
+}
+
+const employeeB = new Employee('조승현', 28, 'developer');
+console.log(employeeB);
+
+// 타입스크립트의 클래스는 타입으로도 활용할 수 있다.
+const employeeC: Employee = {
+    name: '조승현',
+    age: 28,
+    position: '프론트엔드 개발자',
+    work() {
+        console.log('일하는 중')       
+    }
+}
