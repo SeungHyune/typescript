b68157bb0913c2d145c565166dc9f49eda268532
seunghyun <tmdgus7820@naver.com>
2023-11-07 23:29:35 +0900

    feat: typescript section2(기본 타입, tsconfig 속성 정리)

--- onebite-typescript/section1/src/hello.ts
+++ onebite-typescript/section1/src/hello.ts
@@ -1,1 +1,19 @@
-const a = 1;
+const sayHello: string = 'Hello';
+console.log(`${sayHello} world!!`)
+
+const age: number = 20;
+console.log(`나이는 ${age}살 입니다.`);
+
+let age2: 20 = 20;
+// age2 = 21; // number 임에도 할당 불가
+
+let name: '조승현' = '조승현';
+// name = '조승현2'; string 임에도 할당 불가
+
+let name2: string | number = 'string';
+name2 = 20; // string, number 둘 다 할당가능
+
+let nul: null = null;
+//nul = undefined; // null 타입은 null만 가능..
+
+console.log(name2);
