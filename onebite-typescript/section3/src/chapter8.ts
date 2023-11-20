e4f76ca6b2830600a055d5699c73ea392d086fa3
seunghyun <tmdgus7820@naver.com>
2023-11-20 23:45:13 +0900

    feat: typescript section03(타입 좁히기, 서로소 유니온 타입)

--- onebite-typescript/section3/src/chapter8.ts
+++ onebite-typescript/section3/src/chapter8.ts
@@ -4,80 +4,87 @@
  */
 
 type Admin = {
-    tag: 'ADMIN',
-    name: string,
-    kickCount: number
+    tag: 'ADMIN';
+    name: string;
+    kickCount: number;
 };
 
 type Member = {
-    tag: 'MEMBER',
-    name: string,
-    point: number
+    tag: 'MEMBER';
+    name: string;
+    point: number;
 };
 
 type Guest = {
-    tag: 'GUEST',
-    name: string,
-    visitCount: number
+    tag: 'GUEST';
+    name: string;
+    visitCount: number;
 };
 
 type User = Admin | Member | Guest;
 
 // Admin => {name}님 현재까지 {kickCount}명 강퇴했습니다.
-// Member => {name}님 현재까지 {point}모았습니다.
-// Guest => {name}님 현재까지 {visitCount}번 오셨습니다.
+// Member => {name}님 현재까지 {kickCount}명 강퇴했습니다.
+// Guest => {name}님 현재까지 {visitCount}명 강퇴했습니다.
 function login(user: User) {
     switch(user.tag) {
-        case 'ADMIN': {
-            console.log(`${user.name}님 현재까지 ${user.kickCount}명 강퇴했습니다.`);
-            break;
-        }
-        case 'MEMBER': {
-            console.log(`${user.name}님 현재까지 ${user.point} 모았습니다.`);
-            break;
-        }
-        case 'GUEST': {
-            console.log(`${user.name}님 현재까지 ${user.visitCount}번 방문하셨습니다.`);
-            break;
-        }
+        case "ADMIN":
+        console.log(`${user.name}님 현재까지 ${user.kickCount}명 강퇴했습니다.`); 
+        break;
+        case "MEMBER":
+        console.log(`${user.name}님 현재까지 ${user.point} 모았습니다`);
+        break;
+        case "GUEST":
+        console.log(`${user.name}님 현재까지 ${user.visitCount} 방문 했습니다.`);
+        break;
     }
+
+    // if('kickCount' in user) {
+    //     console.log(`${user.name}님 현재까지 ${user.kickCount}명 강퇴했습니다.`);
+    // } else if('point' in user) {
+    //     console.log(`${user.name}님 현재까지 ${user.point} 모았습니다`);
+    // } else if('visitCount' in user) {
+    //     console.log(`${user.name}님 현재까지 ${user.visitCount} 방문 했습니다.`);
+    // }
 }
 
-// 비동기 작업의 결과를 처리하는 객체
-function processResult(task: AsyncTask) {
-    switch(task.state) {
-        case 'LOADING': {
-            console.log('로딩 중');
-            break;
-        }
-        case 'FAILED': {
-            console.log(`에러 발생 : ${task.error.message}`)
-            break;
-        }
-        case 'SUCCESS': {
-            console.log(`처리 완료 : ${task.response.data}`)
-            break;
-        }
-    }
+/**
+ * 비동기 작업의 결과를 처리하는 객체
+ */
+
+type LoadingTask = {
+    state: 'LOADING';
 }
 
-type Loading = {
-    state: 'LOADING',
-}
-
-type Failed = {
-    state: 'FAILED',
+type FailedTask = {
+    state: 'FAILED';
     error: {
-        message: string
-    }
+        message: string;
+    };
 }
 
-type Success = {
-    state: 'SUCCESS',
+type SuccessTask = {
+    state: 'SUCCESS';
     response: {
-        data: string
-    }
+        data: string;
+    };
 }
 
-type AsyncTask = Loading | Failed | Success
+type AsyncTask = LoadingTask | FailedTask | SuccessTask
 
+// 로딩 중 -> 콘솔에 로딩중 출력
+// 실패 -> 콘솔에 실패 에러메세지 출력
+// 성공 -> 콘솔에 성공 데이터 출력
+function processResult(task:AsyncTask){
+    switch(task.state) {
+        case "LOADING":
+        console.log('로딩 중');
+        break;
+        case "FAILED":
+        console.log(`에러 발생: ${task.error.message}`);
+        break;
+        case "SUCCESS":
+        console.log(`성공: ${task.response.data}`);
+        break;
+    }
+}
