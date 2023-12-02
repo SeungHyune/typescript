d234b9c08b7c1796ee3e3a18a437b61de5e0c6ad
seunghyun <tmdgus7820@naver.com>
2023-12-02 12:30:40 +0900

    feat: UI 구조 수정 & todo 수정 기능 추가

--- onebite-typescript/section11/src/components/TodoItem.tsx
+++ onebite-typescript/section11/src/components/TodoItem.tsx
@@ -5,8 +5,9 @@ import { Todo } from '../types/types';
 interface Props extends Todo {}
 
 export default function TodoItem(props: Props) {
-    const todoTextRef = useRef<HTMLDivElement>(null);
+    const todoTextRef = useRef<HTMLInputElement>(null);
     const [isEdit, setIsEdit] = useState(false);
+    const [currentTodoText, setCurrentTodoText] = useState(props.content);
 
     const dispatch = useTodoDispatch();
 
@@ -16,24 +17,39 @@ export default function TodoItem(props: Props) {
         dispatch.onClickDelete(props.id)
     }
 
-    const onTodoTextSave = (e: React.KeyboardEvent<HTMLElement>) => {
-        if(isEdit && todoTextRef.current) {
-            if(e.key === 'Escape') {
-                todoTextRef.current.setAttribute('contenteditable', 'false');
-            }
-        }
+    const onTodoTextEdit = () => {
+        setIsEdit(true);
+        setTimeout(() => {
+            todoTextRef.current?.focus();
+        })
     }
 
-    const onTodoTextUpdate = (e: React.MouseEvent<HTMLElement>) => {
-        if(e.target && todoTextRef.current) {
-            todoTextRef.current.setAttribute('contenteditable', 'true');
-            todoTextRef.current.focus();
-            setIsEdit(true);
+    const onTodoTextUpdate = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setCurrentTodoText(e.target.value);
+    }
+
+    const onTodoUpdate = (e: React.KeyboardEvent<HTMLElement>) => {
+        if(e.key === 'Escape') {
+            setCurrentTodoText(props.content);
+            setIsEdit(false);
+        } else if (e.key === 'Enter') {
+            // props로 전달 받은 수정 함수 호출하면서 매개변수로 id와 변경된 텍스트 전달
+            setIsEdit(false);
         }
     }
     
     return <li className="todo-item">
-        <div onDoubleClick={onTodoTextUpdate} onKeyDown={onTodoTextSave} className="text" ref={todoTextRef}>{props.content}</div>
+        <div className="text">
+            <div className={isEdit === false ? 'view' : ''} onDoubleClick={onTodoTextEdit}><span>{currentTodoText}</span></div>
+            <div className={isEdit ? 'view' : ''}>
+                <input 
+                    onInput={onTodoTextUpdate} 
+                    onKeyDown={onTodoUpdate}
+                    value={currentTodoText} 
+                    type="text" 
+                    ref={todoTextRef} />
+            </div>
+        </div>
         <button onClick={onClickButton}>삭제</button>
     </li>
 }
