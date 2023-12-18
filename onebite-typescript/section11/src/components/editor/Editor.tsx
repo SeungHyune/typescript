b7f9b3a9002f48ada77b92d6a2acc769fb0899af
seunghyun <tmdgus7820@naver.com>
2023-12-18 22:59:04 +0900

    [feat] : input 글자 수 입력 체크(공백 제외 두 글자 이상)

--- onebite-typescript/section11/src/components/editor/Editor.tsx
+++ onebite-typescript/section11/src/components/editor/Editor.tsx
@@ -1,28 +1,40 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useTodoDispatch } from '../../App';
 
 export default function Editor() {
     
     const dispatch = useTodoDispatch();
-
+    const inputRef = useRef(null);
     const [text, setText] = useState('');
-
+    const [warning, setWarning] = useState(false);
     const onChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
         setText(e.target.value);
     }
 
     const onSubmitHandle = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        dispatch?.onClickAdd(text);
-        setText('');
+        if(text.trim().length >= 2) {
+            dispatch?.onClickAdd(text);
+            setText('');
+            setWarning(false);
+        } else {
+            setWarning(true);
+            inputRef.current && (inputRef.current as HTMLInputElement).focus();
+        }
     }
 
     return (
         <div className="editor">
             <form onSubmit={onSubmitHandle}>
-                <input type="text" value={text} onChange={onChangeInput} />
+                <input type="text" value={text} onChange={onChangeInput} ref={inputRef} />
                 <button>추가</button>
             </form>
+            {warning ? <div style={{
+                color: 'red',
+                textAlign: 'center',
+                marginTop: 10,
+                fontStyle: 'italic'
+            }}>공백 없이 두 글자 이상 입력해주세요.</div> : null}
         </div>
     )
 }
